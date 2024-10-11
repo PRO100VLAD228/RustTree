005ff75343b6160d995668c93ddbc906b80f565c
PRO100VLAD228 <kozachenkovlad444@gmail.com>
2024-10-11 14:01:09 +0300

    Update tree

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 fn main() {
-    let cols = 9;
-    let rows = (cols + 1) / 2;
-    let layers = 3;
+    let layers = 10;
+    let numbers = 5;
 
     for layer in 0..layers {
+        let cols = numbers + layer * 2;
+        let rows = (cols + 1) / 2;
+
+        let max_cols = numbers + (layers - 1) * 2;
+        let spaces_before = (max_cols - cols) / 2;
+
         for row in 0..rows {
+            for _ in 0..spaces_before {
+                print!(" ");
+            }
             for col in 0..cols {
                 if col >= (cols / 2) - row && col <= (cols / 2) + row {
                     print!("*");
